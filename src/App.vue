945d5b80a238ec61b4515b03c4ea47ecb172bc6c
finndied <finnlivefx1@mail.ru>
2024-02-17 14:32:02 +0700

    Create header of cards

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import HeaderCard from './components/HeaderCard.vue'
 </script>
 
 <template>
-	
+	<div class="w-full h-screen">
+		<h1 class="text-2xl text ml-6 mt-7 font-bold">Список Задач</h1>
+		<HeaderCard />
+	</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/HeaderCard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex gap-96 text-xl mx-auto mt-10 w-max font-semibold">
+		<div>
+         <div class="p-1 w-64 text-center rounded-md bg-red-700">Важные</div>
+         <div></div>
+      </div>
+
+      <div>
+         <div class="p-1 w-64 text-center rounded-md bg-amber-500">Менее важные</div>
+         <div></div>
+      </div>
+
+      <div>
+         <div class="p-1 w-64 text-center rounded-md bg-lime-600">Мало важные</div>
+         <div></div>
+      </div>
+
+	</div>
+</template>
